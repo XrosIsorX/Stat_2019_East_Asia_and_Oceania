4c67648361aafa1bb85cbc27c1b898dfb9048c79
Inmethink <31432383+Inmethink@users.noreply.github.com>
2019-04-16 17:18:14 +0700

    Update index.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,5 @@
 google.charts.load('current', {
-    'packages':['geochart'],
+    'packages':['geochart','corechart','line'],
     // Note: you will need to get a mapsApiKey for your project.
     // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
     'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
@@ -7,11 +7,16 @@ google.charts.load('current', {
 google.charts.setOnLoadCallback(drawChart);
 
 url = "https://docs.google.com/spreadsheets/d/1v6tnxDsjtZm6RV9zZqr3AWBh_6qk-LrayslweRase2Y/gviz/tq?sheet=All_GDP&headers=1&tq=";
+url2 = "https://docs.google.com/spreadsheets/d/1v6tnxDsjtZm6RV9zZqr3AWBh_6qk-LrayslweRase2Y/gviz/tq?sheet=Tax&headers=1&tq=";
 
 function drawChart() {
     var queryString = encodeURIComponent("select A, B");
     var query = new google.visualization.Query( url + queryString );
     query.send(drawRegionsMap);
+    
+    queryString = encodeURIComponent("select A,B,C,D,E,F");
+    query = new google.visualization.Query(url2 + queryString);
+    query.send(handleTaxResponse);
 }
 
 function errorGen(res) {
@@ -44,4 +49,33 @@ function drawRegionsMap(response) {
     var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
 
     chart.draw(data, options);
+}
+function handleTaxResponse(response) {
+    if (response.isError()) {
+        alert(
+        "Error in query: " +
+            response.getMessage() +
+            " " +
+            response.getDetailedMessage()
+        );
+        return;
+    }
+    var data = response.getDataTable();
+
+
+    var options = {
+        title: "ภาษีของแต่ละประเทศเมื่อเทียบกับรายรับในแต่ละปี",
+        subtitle : "หน่วย $US",
+        vAxis:{
+            title: 'ภาษีที่ต้องจ่าย (USD)'
+        },
+        hAxis: {
+            title: 'รายรับบุคคล (USD)'
+        },
+    };
+
+    var tax_regression = new google.charts.Line(
+        document.getElementById("tax_regression_div")
+    );
+    tax_regression.draw(data, google.charts.Line.convertOptions(options));
 }
